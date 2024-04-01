0d408263642004a8b4b2bc8417cf4c5fc5599a0b
Jeongjin Park <jpark3141@atl1-1-02-018-33-0.pace.gatech.edu>
2024-04-01 18:12:14 -0400

    half successful file rename in plot

--- src/util.py
+++ src/util.py
@@ -1,18 +1,18 @@
 import os
 import re
 
-folder_path = '../plot/Vector_field/train_MLPskip_Jac'
+folder_path = '../plot/Vector_field/train_MLPskip_MSE'
 
 # List all files in the folder
 files = os.listdir(folder_path)
 
 # Define a regular expression pattern to match the number after "Jacobian"
-pattern = r'(\w+_Jacobian)(\d{1,2})'
+pattern = r'\d+'
 
 # Iterate through the files and rename them
 for file in files:
-    if file.startswith('minRE_Comb_Jacobian'):
+    if file.startswith('minRE_Comb'):
         old_name = os.path.join(folder_path, file)
-        new_name = re.sub(pattern, r'\1', old_name)
+        new_name = re.sub(pattern, "", old_name)
         os.rename(old_name, new_name)
 
