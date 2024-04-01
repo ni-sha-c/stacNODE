4c03af7ce19a464253725007aa8976613957b6c2
Jeongjin Park <jpark3141@atl1-1-02-018-33-0.pace.gatech.edu>
2024-04-01 18:39:59 -0400

    finished organizing file names

--- src/util.py
+++ src/util.py
@@ -1,15 +1,15 @@
 import os
 
-folder_path = '/path/to/your/folder'
+folder_path = '../test_result/'
 
 # List all files in the folder
 files = os.listdir(folder_path)
 
 # Iterate through the files and rename them
 for file_name in files:
-    if file_name.startswith('Comb_Jacobian'):
+    if file_name.startswith('Comb'):
         # Extract the part after "Comb_" and remove the number
-        new_file_name = 'Jacobian:' + file_name.split('Comb_Jacobian')[1].split(':')[1]
+        new_file_name = 'MSE:' + file_name.split('Comb')[1].split(':')[1]
         # Create the new file path
         new_path = os.path.join(folder_path, new_file_name)
         # Rename the file
