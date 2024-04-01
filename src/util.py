47cfe5b4cdecaac6444d2c7397d1a673c0589e78
Jeongjin Park <jpark3141@atl1-1-02-018-33-0.pace.gatech.edu>
2024-04-01 18:35:42 -0400

    modifying file names under test_result

--- src/util.py
+++ src/util.py
@@ -1,18 +1,17 @@
 import os
-import re
 
-folder_path = '../plot/Vector_field/train_MLPskip_MSE'
+folder_path = '/path/to/your/folder'
 
 # List all files in the folder
 files = os.listdir(folder_path)
 
-# Define a regular expression pattern to match the number after "Jacobian"
-pattern = r'\d+'
-
 # Iterate through the files and rename them
-for file in files:
-    if file.startswith('minRE_Comb'):
-        old_name = os.path.join(folder_path, file)
-        new_name = re.sub(pattern, "", old_name)
-        os.rename(old_name, new_name)
+for file_name in files:
+    if file_name.startswith('Comb_Jacobian'):
+        # Extract the part after "Comb_" and remove the number
+        new_file_name = 'Jacobian:' + file_name.split('Comb_Jacobian')[1].split(':')[1]
+        # Create the new file path
+        new_path = os.path.join(folder_path, new_file_name)
+        # Rename the file
+        os.rename(os.path.join(folder_path, file_name), new_path)
 
