bf9a76fc95d0ab75f808aeba25e59d77e9fc1164
Nisha Chandramoorthy <nishac@mit.edu>
2024-05-10 15:23:58 -0400

    srb for l63

--- test/test_srb.py
+++ test/test_srb.py
@@ -1,9 +1,14 @@
+import sys
+from matplotlib.pyplot import *
+from numpy import *
+from numba import cuda
+import math
+
 @cuda.jit(device=True)
 def dynamics(x, s):
-    sinx1, sinx2 = sin(x[0]), sin(2*x[1])
-    ceilxpi = math.floor(x[0] / pi) * (2*pi)
-    x[0] = (2 * x[0] + (s[0] + s[1] * sinx2 / 2) * sinx1 - ceilxpi) % (2*pi)
-    x[1] = ((x[1] + (s[3] + s[2] * sinx1) * sinx2 + ceilxpi) * 3) % (2*pi)
+    x[0] = x[0] + 0.005*s[0] * (x[1] - x[0]) 
+    x[1] = x[1] + 0.005*(x[0]*(s[1] - x[2]) - x[1])
+    x[2] = x[2] + 0.005*(x[1]*x[0] - s[2]*x[2])
 
 @cuda.jit
 def compute_srb(n_g, z0, s, srb):
@@ -11,20 +16,19 @@ def compute_srb(n_g, z0, s, srb):
     o_id = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
     n_t = 100
     beg = (o_id - 1) * n_g * n_g
-    pi2 = 2*pi
-    dz = pi2/n_g
+    dz = 40/n_g
     z = z0[o_id]
     for i in range(n_t):
         dynamics(z,s)
-        x, y = z
+        x, y, t = z
         pos_x, pos_y = int(x/dz), int(y/dz)
         pos = beg + pos_x + pos_y * n_g
         srb[pos] += 1/n_t
-
+    
 
 
 def plot_srb(s, ng):
-    pi2, ng2 = 2*pi, ng*ng
+    pi2, ng2 = 40, ng*ng
     dz = pi2/ng
     z = [((i + 0.5)*dz) for i in range(ng)]
     zx, zy = meshgrid(z, z)
@@ -43,10 +47,10 @@ def plot_srb(s, ng):
     srb = srb.copy_to_host()
     srb_g = array([sum(srb[i::ng2])/nt/nrep for i in range(ng2)]).reshape(ng, ng)
     srb_g = srb_g/dz/dz
-
+    print(srb_g.min())
     fig, ax = subplots()
     c = ax.contourf(zx,zy,srb_g)
     cbar = colorbar(c)
     ax.xaxis.set_tick_params(labelsize=24)
     ax.yaxis.set_tick_params(labelsize=24)
-
+    show()
